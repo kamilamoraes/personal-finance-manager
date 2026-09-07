d5e4af667610cb03b4601ee0dbc1d97eca944e50
Kamila Moraes <kamilaamora336@gmail.com>
2026-09-07 20:12:33 -0300

    finaliza operacoes do banco de dados

--- sql/database.sql
+++ sql/database.sql
@@ -69,3 +69,30 @@ WHERE transacoes.tipo = 'despesa'
 GROUP BY categorias.nome;
 
 SELECT * FROM transacoes;
+
+UPDATE transacoes
+SET valor = 460.00
+WHERE id = 3;
+
+SELECT * FROM transacoes
+WHERE id = 3;
+
+INSERT INTO transacoes
+(id, descricao, valor, tipo, data, categoria_id)
+VALUES
+(6, 'Transação de teste', 10.00, 'despesa', '2026-09-06', 5);
+
+SELECT * FROM transacoes
+WHERE id = 6;
+
+DELETE FROM transacoes
+WHERE id = 6;
+
+SELECT * FROM transacoes;
+
+UPDATE transacoes
+SET valor = 450.00
+WHERE id = 3;
+
+SELECT * FROM transacoes;
+
